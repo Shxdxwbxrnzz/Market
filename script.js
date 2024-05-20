4a13680535c2575a8d04df60c47458c9ade89157
Shxdxwbxrnzz <158022767+Shxdxwbxrnzz@users.noreply.github.com>
2024-05-20 09:49:46 +0300

    commit

--- script.js
+++ script.js
@@ -31,54 +31,43 @@ if (itemsDiv) {
 
 //Створення масиву
 let itemsArray = [
-{
-    firstName:"Віталій",
-    lastName:"Шатківський",
-    age:"43",
-    subject:"Інформатика",
-    photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/03/dsc07815.jpg",
-    url:"https://lyceum.ztu.edu.ua/team/shatkivskyy-v-m/",
-},
-{
-    firstName:"Анжела",
-    lastName:"Лознюк",
-    age:"18",
-    subject:"Українська мова",
-    photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/10/photo_2023-10-25_15-46-33-1-693x1024.jpg",
-    url:"https://lyceum.ztu.edu.ua/team/loznyuk-anzhela-leonidivna/",
-},
-{
-    firstName:"Наталія",
-    lastName:"Зинюк",
-    age:"18",
-    subject:"Хімія",
-    photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2024/01/zynjuk-natalija-mykolayivna-683x1024.jpg",
-    url:"https://lyceum.ztu.edu.ua/team/zynyuk-nataliya-mykolayivna/",
-},
-{
-    firstName:"Олена",
-    lastName:"Геча",
-    age:"18",
-    subject:"Історія",
-    photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/hecha-olena-anatoliyivna.jpg",
-    url:"https://lyceum.ztu.edu.ua/team/hecha-o-a/",
-},
-{
-    firstName:"Наталія",
-    lastName:"Кучер",
-    age:"18",
-    subject:"Фізика",
-    photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/kucher-nataliya-viktorivna-683x1024.jpg",
-    url:"https://lyceum.ztu.edu.ua/team/kucher-n-v/",
-},
-{
-    firstName:"Вікторія",
-    lastName:"Нелипович",
-    age:"18",
-    subject:"Математика",
-    photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/nelypovych-viktoriya-vitaliyivna.jpg",
-    url:"https://lyceum.ztu.edu.ua/team/nelypovych-viktoriya-vitaliyivna/",
-},
+    {
+        title: "Мотокоса 43",
+        image: "img/img-3.png",
+        priseNormal: 5499,
+        priseDiscount: 4497,
+        priseCoupon: 4395,
+        lapka: 6,
+        pb: 8,
+    },
+    {
+        title: "Електричний тример 110",
+        image: "https://rebiner.ua/media/catalog/product/cache/d7cfe7d50445e7d0b720212a2e24da17/r/g/rgt850.jpg",
+        priseNormal: 5499,
+        priseDiscount: 4497,
+        priseCoupon: 4395,
+        lapka: 6,
+        pb: 8,
+    },
+    {
+        title: "Електрична газонокосарка 32",
+        image: "img/img-3.png",
+        priseNormal: 5499,
+        priseDiscount: 4497,
+        priseCoupon: 4395,
+        lapka: 6,
+        pb: 8,
+    },
+    {
+        title: "Акумуляторний обприскувач 12 N",
+        image: "img/img-3.png",
+        priseNormal: 5499,
+        priseDiscount: 4497,
+        priseCoupon: 4395,
+        lapka: 6,
+        pb: 8,
+    },
+
 ]
 
 if (itemsDiv) {
@@ -86,13 +75,27 @@ if (itemsDiv) {
         itemsDiv.innerHTML += 
         `
         <div class = "item">
-        <h2>Викладач ${index += 1} з ${itemsArray.length}</h2>
-        <p>${item.firstName} ${item.lastName}</p>
-        <p>Вік: ${item.age}</p>
-        <p>Викладає: ${item.subject}</p>
-        <p><img src = "${item.photo}" class="item-image"></p>
-        <p><a href = "${item.url}" target="_blank">Профіль</p>
+        <div class="item-title">${item.title}</div>
+              
+        <div class="item-image">
+        <img src="${item.image}" alt="Мотокоса">
         </div>
+        
+        <div class="parts-pay">
+            <div><img src="https://proger.com.ua/upload/webp_cache/iblock/099/t86mp7yql1f8xfeihmxoewgg624f2sc7/lapka.webp" alt="mono">${item.lapka}</div>
+            <div><img src="img/img-2.png" alt="pie">${item.pb}</div>
+        </div>
+        
+        <div class="price">
+          <div><span>${item.priseNormal}</span><sup>грн</sup></div>
+          <div><span>${item.priseDiscount}</span><sup>грн</sup></div>
+        </div>
+        
+        <div class="price bonus">
+          <div>Ціна за купоном</div>
+          <div><span>${item.priseCoupon}</span><sup>грн</sup></div>
+        </div> 
+
         `
     })
 }
@@ -107,3 +110,78 @@ console.log (itemsArray)
 for(let i = 0; i < itemsArray.lenght; i++) {
     console.log (i + "-й елемент:", itemsArray[i])
 }
+
+
+
+
+// Вчителі
+
+// {
+//     firstName:"Віталій",
+//     lastName:"Шатківський",
+//     age:"43",
+//     subject:"Інформатика",
+//     photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/03/dsc07815.jpg",
+//     url:"https://lyceum.ztu.edu.ua/team/shatkivskyy-v-m/",
+// },
+// {
+//     firstName:"Анжела",
+//     lastName:"Лознюк",
+//     age:"18",
+//     subject:"Українська мова",
+//     photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/10/photo_2023-10-25_15-46-33-1-693x1024.jpg",
+//     url:"https://lyceum.ztu.edu.ua/team/loznyuk-anzhela-leonidivna/",
+// },
+// {
+//     firstName:"Наталія",
+//     lastName:"Зинюк",
+//     age:"18",
+//     subject:"Хімія",
+//     photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2024/01/zynjuk-natalija-mykolayivna-683x1024.jpg",
+//     url:"https://lyceum.ztu.edu.ua/team/zynyuk-nataliya-mykolayivna/",
+// },
+// {
+//     firstName:"Олена",
+//     lastName:"Геча",
+//     age:"18",
+//     subject:"Історія",
+//     photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/hecha-olena-anatoliyivna.jpg",
+//     url:"https://lyceum.ztu.edu.ua/team/hecha-o-a/",
+// },
+// {
+//     firstName:"Наталія",
+//     lastName:"Кучер",
+//     age:"18",
+//     subject:"Фізика",
+//     photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/kucher-nataliya-viktorivna-683x1024.jpg",
+//     url:"https://lyceum.ztu.edu.ua/team/kucher-n-v/",
+// },
+// {
+//     firstName:"Вікторія",
+//     lastName:"Нелипович",
+//     age:"18",
+//     subject:"Математика",
+//     photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/nelypovych-viktoriya-vitaliyivna.jpg",
+//     url:"https://lyceum.ztu.edu.ua/team/nelypovych-viktoriya-vitaliyivna/",
+// },
+
+
+{/* <div class="item-title">${item.title}</div>
+              
+<div class="item-image"></div>
+    <img src="${item.image}" alt="Мотокоса">
+
+<div class="parts-pay">
+    <div><img src="https://proger.com.ua/upload/webp_cache/iblock/099/t86mp7yql1f8xfeihmxoewgg624f2sc7/lapka.webp" alt="mono">${item.lapka}</div>
+    <div><img src="img/img-2.png" alt="pie">${item.pb}</div>
+</div>
+
+<div class="prise">
+  <div><span>${item.priseNormal}</span><sup>грн</sup></div>
+  <div><span>${item.priseDiscount}</span><sup>грн</sup></div>
+</div>
+
+<div class="prise bonus">
+  <div>Ціна за купоном</div>
+  <div><span>${item.priseCoupon}</span><sup>грн</sup></div>
+</div> */}
